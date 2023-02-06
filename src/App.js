df0c9b4cd0cadc949d3d3df16b6e4b34d904375c
C-dub <25037768+ColeS82@users.noreply.github.com>
2023-02-06 14:46:07 -0700

    adding initial files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Materialize from 'materialize-css'
+import './App.css';
+import Home from './components/Home/Home'
+
+function App() {
+  return (
+    <Home />
+  );
+}
+
+export default App;
